527bd42c0a2d77af4eb21d94b49273ca367427cc
muxe <max@apeunit.com>
2017-09-22 10:40:18 +0200

    truflle, token v2 test

--- contract/migrations/2_deploy_contracts.js
+++ contract/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var AEProof = artifacts.require("./AEProof.sol");
+var AEProof2 = artifacts.require("./AEProof2.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(AEProof);
+  deployer.deploy(AEProof2);
 };
