cef50302ed98b3687a63efd53d3ec9449b242e1f
Shamari Feaster <alistproducer2@gmail.com>
2017-10-27 01:09:17 -0400

    added ipfs

--- src/mixins/helper.js
+++ src/mixins/helper.js
@@ -1,8 +1,35 @@
 import moment from 'moment'
 const blockies = require('ethereum-blockies-png')
+const ipfsAPI = require('ipfs-api')
 
 export default {
 	methods: {
+		getIpfsClient: function (host = 'localhost', port = 5001, protocol = 'http') {
+			return ipfsAPI(host, port, {protocol: protocol})
+		},
+		getIpfsContent: function (ipfsHash, callback, errCb) {
+			let ipfs = this.getIpfsClient()
+			ipfs.files.get(ipfsHash, function (err, stream) {
+				if (err) {
+					errCb(err)
+				}
+				stream.on('data', (file) => {
+					// write the file's path and contents to standard out
+					console.log(file.path)
+					callback(file.content)
+				})
+			})
+		},
+		addIpfsContent: function (content, callback, errCb) {
+			let ipfs = this.getIpfsClient()
+			ipfs.add([content], (err, res) => {
+				if (err) {
+					errCb(err)
+					throw err
+				}
+				callback(res[0].hash)
+			})
+		},
 		readableTimestamp: function (timestamp) {
 			if (timestamp) {
 				let momentTime = moment.unix(timestamp.toString())
